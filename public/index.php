66bd7eae43d1e5d68ae14d4e5840e5aec94e1b9e
Sujane Panjan <sujane@Sujanes-MacBook-Air.local>
2025-08-08 14:27:56 +0700

    Update: Simplify index.php (remove phpinfo) and change Redis auth to php84

--- public/index.php
+++ public/index.php
@@ -1,3 +1,6 @@
 <?php
-phpinfo();
+echo "<h1>Hello World!</h1>";
+echo "<p>This is a pure PHP 8.4 application running on Nginx port 9000</p>";
+echo "<p>Current PHP version: " . PHP_VERSION . "</p>";
+echo "<p>Current time: " . date('Y-m-d H:i:s') . "</p>";
 ?>
