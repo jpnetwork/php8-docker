c1750e1eee79f42f3be0cd0515aec457777f6462
Sujane Panjan <sujane@Sujanes-MacBook-Air.local>
2025-08-10 12:42:28 +0700

    Testtt

--- public/index.php
+++ public/index.php
@@ -1,5 +1,5 @@
 <?php
-echo "<h1>Hello World!</h1>";
+echo "<h1>Hello World! Please help MEEEEEE</h1>";
 echo "<p>This is a pure PHP 8.4 application running on Nginx port 9000</p>";
 echo "<p>Current PHP version: " . PHP_VERSION . "</p>";
 echo "<p>Current time: " . date('Y-m-d H:i:s') . "</p>";
