45724df9f799ec11fbfba0a5c6c61c3483717e1a
Sujane Panjan <sujane@Sujanes-MacBook-Air.local>
2025-08-12 14:49:59 +0700

    delete some files

--- public/index.php
+++ public/index.php
@@ -1,6 +1,6 @@
 <?php
-echo "<h1>Hello World! Please help MEEEEEE</h1>";
-echo "<p>This is a pure PHP 8.4 application running on Nginx port 9000</p>";
+echo "<h1>Hello World! Please help MEEEEE</h1>";
+echo "<p>This is a pure PHP 8.4 application running on Nginx</p>";
 echo "<p>Current PHP version: " . PHP_VERSION . "</p>";
 echo "<p>Current time: " . date('Y-m-d H:i:s') . "</p>";
 ?>
